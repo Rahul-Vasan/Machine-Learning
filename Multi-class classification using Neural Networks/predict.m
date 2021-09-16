a12a4f62bf0215c4a885bc5f0598ad785ef16732
Rahul-Vasan <90875442+Rahul-Vasan@users.noreply.github.com>
2021-09-16 19:44:12 -0400

    Multi-class classification using Neural networks
    Problem Statement:
    
    you will have to use logistic regression and neural networks to
    recognize handwritten digits (from 0 to 9). Automated handwritten digit
    recognition is widely used today - from recognizing zip codes (postal codes)
    on mail envelopes to recognizing amounts written on bank checks.
    
    Outcome: Learnt implementing neural networks and used fminunc for efficient gradient calculation.

---
+++ Multi-class classification using Neural Networks/predict.m
@@ -0,0 +1,38 @@
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+X = [ones(m, 1) X];
+A = X*Theta1';
+Z1= sigmoid(A);
+q = size(Z1,1);
+Z1 = [ones(q, 1) Z1];
+A2 = Z1*Theta2';
+Z2 = sigmoid(A2);
+[index,p] = max(Z2,[],2);
+
+
+
+
+% =========================================================================
+
+
+end
